8bdd6e67de37cd7c322201cd405284ddf61fc2cb
Henning-Winker <henning.winker@gmail.com>
2022-10-05 14:22:31 +0200

    fix B0 -inf issue

--- R/ComputeRefs.R
+++ R/ComputeRefs.R
@@ -153,7 +153,7 @@ computeFbrp <- function(stock,sr='missing',proxy=NULL,x=NULL,blim=0.1,type=c("b0
     refs= rbind(refs,FLPar(Bpa=bpa))
   }
   
-  refs=rbind(refs,FLPar(B0=B0))
+  refs=rbind(refs,FLPar(B0=0.99*B0))
   
   # do check
   check = brp+refs
@@ -162,6 +162,7 @@ computeFbrp <- function(stock,sr='missing',proxy=NULL,x=NULL,blim=0.1,type=c("b0
   }
     
   brp = brp+refs
+  refpts(brp)["B0"] = refpts(brp)["virgin"]
   #if(fmax*refpts(check)["Fbrp","harvest"]>max(fbar(brp))){
     fl = min(c(fmax*refpts(check)[paste(fref),"harvest"],refpts(check)["Blim","harvest"]*1.5))
     fbar(brp) = seq(0,fl,fl/100)
